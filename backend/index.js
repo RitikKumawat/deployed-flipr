5a732e9a9b97094c4136c424e2149d7fc3007d35
RitikKumawat <rtkkmwt@gmail.com>
2024-05-06 12:19:58 +0530

    chng

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://deployed-flipr.vercel.app",
     credentials:true
 }
 ));
--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000/api"
+const BASE_URL = "https://backend-qllu.onrender.com"
 
 export const authEndpoints = {
     SIGNUP_API: BASE_URL + "/auth/signup",
